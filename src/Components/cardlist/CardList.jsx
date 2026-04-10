3bfa8f53547d64c93b6f327d141100246ceb0faf
JEFFERSON DIEGO LIMA DA SILVA <jeffersonsilva0425@digitalcollege.internal>
2026-04-09 21:56:12 -0300

    Cards

---
+++ src/Components/cardlist/CardList.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react'
+
+export const CardList = () => {
+
+const getMoviews = async () => {
+    try {
+        const config = {
+            //method: 'GET',
+            headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OGU3MDFkYjNmNTUyZTBhNTFjMDlkNDMxMzdiZDI3MCIsIm5iZiI6MTY4ODczMDA1NC44NzgsInN1YiI6IjY0YTdmOWM2OTY1MjIwMDExZGYwOGU3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YhX8YDb0OF8ovacEzdWjUTSWr0xZLaZOItyxsnzgVMI'
+    }
+        }
+    }
+}
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { CardList } from '../Components/cardlist/CardList'
 
 export const Home = () => {
   return (
     <div>
-      Home
+      <CardList />
     </div>
   )
 }
